66a30298e2860bfbd237c895e4f0bdaf4146154c
Aaron Altounian <aaron.altounian@gmail.com>
2019-05-02 16:25:56 -0500

    did 'find non repeating' whiteboard

--- find-non-repeating.js
+++ find-non-repeating.js
@@ -8,22 +8,23 @@ return 4.
 */
 //
 function findNonRepeating(theWord){
-    //whats the secret of strings
-    let count = 0;
-    for( let i = 0; i < theWord.length; i++ ) {
-      count = 0;
-      for( let j = 1; j < theWord.length; j++ ) {
-        if( i != j && theWord[i] === theWord[j] ) {
-          count++;
-          
-        }
+
+  let count = 0;
+  for( let i = 0; i < theWord.length; i++ ) {
+    count = 0;
+    for( let j = 1; j < theWord.length; j++ ) {
+
+      if( i != j && theWord[i] === theWord[j] ) {
+        count++;
       }
-      if( count === 0 ) {
-        return i;
-      }
+
+    }
+
+    if( count === 0 ) {
+      return i;
     }
 
   }
-  console.log(findNonRepeating("lollipop"));
-  console.log(findNonRepeating("seeeeat"));
-  
+}
+console.log(findNonRepeating("lollipop"));
+console.log(findNonRepeating("seeeeat"));
